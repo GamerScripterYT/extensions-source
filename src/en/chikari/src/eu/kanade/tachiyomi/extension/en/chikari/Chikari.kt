c9f3e41e930744a01af925cc7f73eb888617e932
シャドーイ <152271448+GamerScripterYT@users.noreply.github.com>
2026-09-16 00:35:56 +0800

    Fix Spotless formatting violations - reorganize imports

--- src/en/chikari/src/eu/kanade/tachiyomi/extension/en/chikari/Chikari.kt
+++ src/en/chikari/src/eu/kanade/tachiyomi/extension/en/chikari/Chikari.kt
@@ -9,16 +9,16 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.util.asJsoup
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.concurrent.TimeUnit
 
 class Chikari : ParsedHttpSource() {
 
